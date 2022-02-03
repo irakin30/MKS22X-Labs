e7fae925f84c830068026253fadec24997b2a92d
Istab Rakin <64758911+irakin30@users.noreply.github.com>
2022-02-03 18:23:36 -0500

    v1.1 printNoDoubleLetterWords added

--- 01-recursion/Recursion.java
+++ 01-recursion/Recursion.java
@@ -1,12 +1,13 @@
 public class Recursion {
-          /*Print all words that are made of the letters a-e inclusive.
+          
+         /*Print all words that are made of the letters a-e inclusive.
           *@param length : the length of the words that are to be printed
           */
           public static void printAllWords(int length){
             printAllWords(length,"");
           }
 
-          /*Print all words that are made of the letters a-e inclusive.
+         /*Print all words that are made of the letters a-e inclusive.
           *@param length : either how many more letters or the total length depending on implementation
           *@param word   : the partial word so far.
           */
@@ -21,6 +22,50 @@ public class Recursion {
                 }
             }
           }
+          
+          /*
+           * Print all words that are made of the characters in the array of letters.
+           * There may not be consecutive equal letters, so:
+           * aax is not allowed, but axa is allowed.
+           * 
+           * @param length : the length of the words that are to be printed
+           * 
+           * @param letters: the letters you should be using,
+           * 
+           * @precondition: letters contains at least 2 characters, and has no duplicates.
+           */
+          public static void printNoDoubleLetterWords(int length, char[] letters) {
+              printNoDoubleLetterWords(length, "", letters);
+          }
+
+          /*
+           * Print all words that are made of the characters in letters. There may not be
+           * consecutive equal letters,
+           * aax is not allowed, but axa is allowed.
+           * 
+           * @param length : either how many more letters need to be
+           * 
+           * @param word : the partial word so far.
+           * 
+           * @param letters: the letters you should be using
+           */
+          public static void printNoDoubleLetterWords(int length, String word, char[] letters) {
+              // WRITE THIS METHOD
+              if (length == 0) {
+                    System.out.println(word); 
+              } 
+              else {
+                 for(char c : letters) {
+                    if(word.length() == 0) {
+                        printNoDoubleLetterWords(length - 1, word + c, letters);
+                    } 
+                    else if(c != word.charAt(word.length() - 1)) {
+                        printNoDoubleLetterWords(length - 1, word + c, letters); 
+                    }
+                 }
+              }
+          }
+
 
           public static void main(String args[]) {
               printAllWords(3); 
