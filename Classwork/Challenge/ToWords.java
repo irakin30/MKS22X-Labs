050542a92358114d1f1c402a7505e680b9e6a428
Istab Rakin <irakin30@stuy.edu>
2022-02-08 11:20:46 -0500

    Backup

---
+++ Classwork/Challenge/ToWords.java
@@ -0,0 +1,6 @@
+public class ToWords{
+    public static String intToWords(int n) {
+        String[] unique = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "seventeen", "nineteen"};
+        String[] prefix = {"twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty"};
+    }
+}
