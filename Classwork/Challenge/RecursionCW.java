167f532671b400bdb651b934ff1a30c52925a50e
Istab Rakin <irakin30@stuy.edu>
2022-02-09 11:43:04 -0500

    RecursionClasswork made code look nicer

--- Classwork/Challenge/RecursionCW.java
+++ Classwork/Challenge/RecursionCW.java
@@ -6,7 +6,8 @@ public class RecursionCW {
     public static boolean groupSum(int start, int[] nums, int target) {
         if (target == 0) return true;
         if (start < nums.length && target > 0) {
-            return groupSum(start + 1, nums, target - nums[start]) || groupSum(start + 1, nums, target);
+            return groupSum(start + 1, nums, target - nums[start]) ||
+                   groupSum(start + 1, nums, target);
         }
         return false;
     }
@@ -17,18 +18,36 @@ public class RecursionCW {
 
     public static boolean splitArray(int[] nums, int start, int sumA, int sumB) {
         if (start == nums.length) return sumA == sumB;
-        return splitArray(nums, start + 1, sumA + nums[start], sumB ) || splitArray(nums, start + 1, sumA , sumB + nums[start]);
+        return splitArray(nums, start + 1, sumA + nums[start], sumB ) ||
+               splitArray(nums, start + 1, sumA , sumB + nums[start]);
     }
 
-    public boolean split53(int[] nums) {
+    public static boolean split53(int[] nums) {
       return split53(nums, 0, 0, 0);
     }
 
-    public boolean split53(int[] nums, int start, int sumA, int sumB) {
+    public static boolean split53(int[] nums, int start, int sumA, int sumB) {
         if (start == nums.length) return sumA == sumB;
         if (nums[start] % 5 == 0) return split53(nums, start + 1, sumA + nums[start], sumB);
         if (nums[start] % 3 == 0) return split53(nums, start + 1, sumA , sumB + nums[start]);
         return split53(nums, start + 1, sumA + nums[start], sumB ) || split53(nums, start + 1, sumA , sumB + nums[start]);
+    }
+
+    //groupSum6(0, [5, 2, 4, 6], 9) → false
+    //fails groupSum6(0, [1, 6, 2, 6, 4], 9) → false
+    //fails groupSum6(0, [3, 2, 4, 6], 3) → false
+    public static boolean groupSum6(int start, int[] nums, int target) {
+        if (target == 0) return true;
+        if (start < nums.length && target > 0) {
+            //6 must be chosen, therefore the if statement
+            if (nums[start] == 6) return groupSum6(start + 1, nums, target - nums[start]) ;
+            return groupSum6(start + 1, nums, target - nums[start]) || groupSum6(start + 1, nums, target);
+        }
+        return false;
+    }
+
+    public static boolean splitOdd10(int[] nums) {
+
     }
 
     public static void main(String[] args) {
@@ -45,19 +64,5 @@ public class RecursionCW {
         System.out.println(splitArray(new int[] {2, 3})); //false
         System.out.println(splitArray(new int[] {5, 2, 3})); //true
         System.out.println(splitArray(new int[] {10, 2, 3, 1})); //false
-    }
-
-    //groupSum6(0, [5, 2, 4, 6], 9) → false
-    //fails groupSum6(0, [1, 6, 2, 6, 4], 9) → false
-    //fails groupSum6(0, [3, 2, 4, 6], 3) → false
-
-    public static boolean groupSum6(int start, int[] nums, int target) {
-        if (target == 0) return true;
-        if (start < nums.length && target > 0) {
-            //6 must be chosen, therefore the if statement
-            if (nums[start] == 6) return groupSum6(start + 1, nums, target - nums[start]) ;
-            return groupSum6(start + 1, nums, target - nums[start]) || groupSum6(start + 1, nums, target);
-        }
-        return false;
     }
 }
