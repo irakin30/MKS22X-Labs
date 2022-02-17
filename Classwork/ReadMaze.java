65cd32e4b3c9f37b98cf58fa4a20e52510ded3e4
Istab Rakin <irakin30@stuy.edu>
2022-02-17 11:33:57 -0500

    created ReadMaze.java

---
+++ Classwork/ReadMaze.java
@@ -0,0 +1,12 @@
+import java.io.File;
+  import java.io.FileNotFoundException;
+  import java.util.Scanner;
+  public class ReadMaze {
+      public static void main(String[] args) {
+        int x  = 32;
+        while(x--) {
+          System.out.print(1);
+        }
+      }
+
+  }
