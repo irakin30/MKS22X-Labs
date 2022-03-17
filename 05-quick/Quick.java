1e90f522504a46315bb9541a0301c85f37f37d36
Istab Rakin <irakin30@stuy.edu>
2022-03-16 23:17:48 -0400

    done with quicksort, tested and comparted to normal Arrays.sort. Next steps: test for efficiency

--- 05-quick/Quick.java
+++ 05-quick/Quick.java
@@ -1,6 +1,6 @@
 import java.io.*;
 import java.util.*;
-public class Preliminary{
+public class Quick{
     /**
      * * Modify the dataay such that:
      * 1. A random index from start to end inclusive is chosen, the
@@ -16,7 +16,7 @@ public class Preliminary{
      * @return the index of the final position of the pivot element.
      */
 
-    public static int partition_l(int data[], int start, int end) {
+    public static int partition(int data[], int start, int end) {
         int pivot = ((int) (Math.random() * (end - start))) + start;
         swap(data, pivot, end);
         pivot = data[end];
@@ -29,6 +29,10 @@ public class Preliminary{
         }
         swap(data, i + 1, end);
         return i + 1;
+    }
+
+    public static int partition_H(int[] data, int high, int low) {
+        return 0; 
     }
 
     private static void swap(int[] data, int indexA, int indexB) {
@@ -59,12 +63,62 @@ public class Preliminary{
     *@param k is 0 to data.length-1 inclusive
     *@postcondition The array may be modified.
     */
+
+    //Wrapper Method
    public static int quickselect(int []data, int k){
-     return 0;
+        return quickselect(data, 0, data.length - 1, k); 
+   }
+
+   //Actual Method - recursively; 
+   private static int quickselect(int[] data, int start, int end, int k) {
+        int partition = partition(data, start, end); 
+        if (partition == k) return partition; 
+        if (partition < k) {
+            return quickselect(data, start, partition - 1, k); 
+        }
+        else {
+            return quickselect(data, partition + 1, end, k);
+        }
+   }
+
+   //Wrapper method; 
+   public static void quicksort(int[] data) {
+       quicksort(data, 0, data.length - 1);
+   }
+
+   public static void quicksort(int[] data, int lo, int hi) {
+       //Valid indexes and not equal so arrays of length 0 don't get made over and over
+       if (lo < hi) {
+            int partition = partition(data, lo, hi); 
+            //left side of the array
+            quicksort(data, lo, partition - 1);
+            //ignore the middle hence -1, since it is in the coorect spot
+            //right side
+            quicksort(data, partition + 1, hi);
+       }
+   }
+
+   public static int[] partitionDutch(int[] arr, int low, int high) {
+       return new int[0]; 
+   }
+
+   private static int[] randArray(int size) {
+        int[] arr = new int[size]; 
+        Random rand = new Random(); 
+        for(int i = 0; i < arr.length; i++) {
+            arr[i] = rand.nextInt();
+        }
+        return arr; 
    }
 
    public static void main(String[] args) {
-      for(int[] data : )
+      for(int i = 0; i < 100; i++) {
+          int[] sortA = randArray(1000000); 
+          int[] sortB = sortA.clone();
+          Arrays.sort(sortA);
+          quicksort(sortB);
+          System.out.println(Arrays.equals(sortA, sortB));
+      }
    }
 
 }
